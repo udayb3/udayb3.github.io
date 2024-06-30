9c9c961119f01d69eeebd481f247041818fe1f56
Uday Bhardwaj <udaybh07@gmail.com>
2024-06-30 13:28:54 +0530

    Adding the script configuration files

---
+++ static/app.js
@@ -0,0 +1,28 @@
+// SubTask1
+
+const board = document.getElementById("board");
+const cells = document.getElementsByClassName("cell");
+const msg = document.getElementById("msg");
+const players = ["X", "O"];
+let current = players[0];
+
+// // add event listeners to each cell
+
+function handleCellClick(event) {
+  // Handle Cell Clicking Functionality
+}
+
+// SubTask2
+
+function checkWin(current) {
+  // // Check Winning conditions
+}
+
+function checkTie() {
+  // // Check Tie conditions
+}
+
+// SubTask3
+function restart() {
+  // // Restart Game Functionality
+}
