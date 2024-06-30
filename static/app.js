ef7d7462d314e49ed0cc9562d36afdc5aa007c8b
Uday Bhardwaj <udaybh07@gmail.com>
2024-06-30 14:52:39 +0530

    Game v-1.0

--- static/app.js
+++ static/app.js
@@ -3,26 +3,85 @@
 const board = document.getElementById("board");
 const cells = document.getElementsByClassName("cell");
 const msg = document.getElementById("msg");
+const restartbtn= document.getElementById
 const players = ["X", "O"];
-let current = players[0];
+let NotFin=true;
+
+let cur = 0; let coun=0
+real_board= [ [-1, -1, -1], [ -1, -1, -1], [ -1, -1 , -1] ]
 
 // // add event listeners to each cell
+function handleCellClick( event ) {
 
-function handleCellClick(event) {
-  // Handle Cell Clicking Functionality
+  tar= event.target; id_= parseInt( (event.target.id)[4] )
+  let i= parseInt(id_/3), j=parseInt( id_%3 );
+
+  if( NotFin && ( real_board[ i ][ j ] ) == -1  ){
+
+    tar.innerHTML ="<h1>"+players[cur] +"</h1>";
+    real_board[ i ][ j ]= cur;
+
+    coun++;
+    if( checkWin( cur ) ){
+      msg.innerHTML= players[cur]+" won the game. Play again by clicking restart"; NotFin= false;
+    }
+    else if( coun==9 ){
+      msg.innerHTML= "The Game has tied. Play again by clicking restart"; NotFin= false;
+    }
+    else{
+      cur= cur==0?1:0;
+      msg.innerHTML=players[cur]+"'s turn"
+    }
+  }
 }
+
+let i=0
+for( i=0;i< cells.length; i++){
+  cells[i].addEventListener("click", handleCellClick )
+}
+
 
 // SubTask2
 
-function checkWin(current) {
-  // // Check Winning conditions
-}
+function checkWin( current ) {
+  let chdl= true, chdr= true;
 
-function checkTie() {
-  // // Check Tie conditions
+  for(let i=0;i<3;i++ ){
+    let ch1=true, ch2= true;
+
+    for( let j=0; j<3; j++){
+      if( current != real_board[ i][ j ] ){
+        ch1= false;
+      }
+
+      if( current != real_board[ j][i] ){
+        ch2= false;
+      }
+    }
+    if( ch1 || ch2 )
+      return true;
+
+    if( current != real_board[i][i] )
+      chdl=false;
+    if(current != real_board[i][2-i] )
+      chdr= false;
+  }
+
+  if( chdl || chdr )
+    return true;
+
+  return false;
 }
 
 // SubTask3
 function restart() {
-  // // Restart Game Functionality
-}
+
+  cur=0;
+  real_board= [ [-1, -1, -1], [-1, -1, -1], [-1, -1, -1]];
+  msg.innerHTML= "X's turn"
+
+  for( let i=0; i<cells.length; i++ ){
+    cells[i].innerHTML= "";
+  }
+
+  }
